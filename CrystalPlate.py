b3ec5963a5c2c91e27571de2c13af74b71e1c529
Alan Zhang <lukanite@gmail.com>
2016-07-16 19:38:54 +0000

    Neater code for docs

--- CrystalPlate.py
+++ CrystalPlate.py
@@ -9,6 +9,13 @@ from pprint import pprint
 import Adafruit_CharLCD as LCD
 from textwrap import fill
 
+#Configuration
+rootCAPath = "certs/rootCA.pem"
+certFilePath = "certs/63fee16d41-certificate.pem.crt"
+keyFilePath = "certs/63fee16d41-private.pem.key"
+iotThing = "Zorua"
+clientID = "Zorua"
+
 #Device JSON initialization
 device = {'state': {'reported': {'HP':100} } }
 device['state']['reported']['color'] = {'r':0, 'g':0, 'b':0}
@@ -40,8 +47,8 @@ def on_connect(mqttc, obj, flags, rc):
     print "Connecting..."
     if rc==0:
         print ("Subscriber Connection status code: "+str(rc)+" | Connection status: successful")
-        mqttc.subscribe("$aws/things/Zorua/shadow/update/delta", qos=1) #The names of these topics start with $aws/things/thingName/shadow."
-        #mqttc.publish("$aws/things/Zorua/shadow/update", json.dumps(device))
+        #We only want to be notified about things we need to change to stay in sync with AWS
+        mqttc.subscribe("$aws/things/" + iotThing + "/shadow/update/delta", qos=1)
     elif rc==1:
         print ("Subscriber Connection status code: "+str(rc)+" | Connection status: Connection refused")
         print ("Subscriber Connection status code: "+str(rc))
@@ -51,21 +58,25 @@ def on_subscribe(mqttc, obj, mid, granted_qos):
     print("Subscribed: "+str(mid)+" "+str(granted_qos)+"data"+str(obj))
     set_lcd_color(0,1,0)
     set_lcd_message('Connected!\nReady for input')
-    mqttc.publish("$aws/things/Zorua/shadow/update", json.dumps(device))
+    #Let AWS know about the current state of the plate so we can tell us what's out of sync
+    mqttc.publish("$aws/things/" + iotThing + "/shadow/update", json.dumps(device))
+
 #called when a message is received by a topic
+#Messages are formatted in JSON
+#When working with /update, we might not find all keys all the time, so we need to handle that
 def on_message(mqttc, obj, msg):
-    #pprint(msg)
     try:
         data = json.loads(msg.payload)
         update = data['state']
     except:
         return
+    #Look for a message in the update. If it's there, we need to update the display
     if 'msg' in update.keys():
         try:
             set_lcd_message(update['msg'])
-            mqttc.publish("$aws/things/Zorua/shadow/update", json.dumps(device))
         except:
             print("Could not enact message from topic: "+msg.topic+" | QoS: "+str(msg.qos)+" | Data Received: "+str(msg.payload))
+    #Look to see if the status of R, G, or B has changed for the display
     if 'color' in update.keys():
         try: lcd_r = update['color']['r']
         except: lcd_r = device['state']['reported']['color']['r']
@@ -76,10 +87,11 @@ def on_message(mqttc, obj, msg):
         set_lcd_color(lcd_r,
                       lcd_g,
                       lcd_b)
-        mqttc.publish("$aws/things/Zorua/shadow/update", json.dumps(device))
+    #Let AWS know we've updated the display
+    mqttc.publish("$aws/things/Zorua/shadow/update", json.dumps(device))
         
 #creating a client with client-id=Zorua
-mqttc = mqtt.Client(client_id="Zorua")
+mqttc = mqtt.Client(client_id=clientID)
 
 mqttc.on_connect = on_connect
 mqttc.on_reconnect = on_connect
@@ -88,9 +100,9 @@ mqttc.on_message = on_message
 
 #Configure network encryption and authentication options. Enables SSL/TLS support.
 #adding client-side certificates and enabling tlsv1.2 support as required by aws-iot service
-mqttc.tls_set("certs/rootCA.pem",
-	            certfile="certs/63fee16d41-certificate.pem.crt",
-	            keyfile="certs/63fee16d41-private.pem.key",
+mqttc.tls_set(rootCAPath,
+	            certfile=certFilePath,
+	            keyfile=keyFilePath,
               tls_version=ssl.PROTOCOL_TLSv1_2, 
               ciphers=None)
 
